f03c0a96161e772a3132a12c5daee3ec5a70497f
Sophiya-ai <sophiyabr@gmail.com>
2024-12-25 13:54:58 +0200

    cw db

--- ZeroCoder/NewApp/admin.py
+++ ZeroCoder/NewApp/admin.py
@@ -1,3 +1,5 @@
-from django.contrib import admin
-
+from django.contrib import admin #импорт класса
+from .models import New_post #
 # Register your models here.
+
+admin.site.register(New_post) #команда регистрации новой таблицы внутри проекта
--- ZeroCoder/NewApp/views.py
+++ ZeroCoder/NewApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import New_post  #импортируем класс
 
 # Create your views here.
 def home(request):
-    return render(request,'NewApp/news.html')
+    news = New_post.objects.all()   #Создаём переменную для получения всех записей
+    return render(request,'NewApp/news.html', {'news' : news})  #Прописываем словарь для передачи информации в html-шаблон
